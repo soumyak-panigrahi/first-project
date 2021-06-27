f829e93f6586da5942bd39b34f38f534b48984ca
soumyak-panigrahi <80380003+soumyak-panigrahi@users.noreply.github.com>
2021-06-27 18:17:54 +0530

    Added new printing Message to main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,7 @@
 int main()
 {
     std::cout<<"Hello World"<<std::endl;
+    std::cout<<"I'm Learening GIT and GITHUB"<<std::endl;
+    std::cout<<"GOOD BYE!!!!!!! World"<<std::endl;
     return 0;
 }
