a9872624e12137c1220ccf899d89a9da8d317178
Soumyak Panigrahi <soumyakpanigrahi123@gmail.com>
2021-06-27 18:27:05 +0530

    Changing to Message in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-    std::cout<<"Hello World"<<std::endl;
-    std::cout<<"I'm Learening GIT and GITHUB"<<std::endl;
-    std::cout<<"GOOD BYE!!!!!!! World"<<std::endl;
+    std::cout << "Hello World" << std::endl;
+    std::cout << "I'm Learning GIT and GITHUB" << std::endl;
+    std::cout << "GOOD BYE!!!!!!! World" << std::endl;
     return 0;
 }
